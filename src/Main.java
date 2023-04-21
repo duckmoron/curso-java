57f443c4c8fe2358cf44003ac40a7875d5fe1049
duckmoron <patriciomoron@gmail.com>
2023-04-21 00:09:21 -0300

    Puntos por partido, puntos extra rondas/fases completas.

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
 
         // configuro para obtener formato tipo tabla
-        String formatoTablaResultados = "%-10s | %-10s | %18s | %-18s | %15s | %-15s | %10s %n";
+        String formatoTablaResultados = "%-10s | %-10s | %18s | %-18s | %15s | %-15s | %15s %n";
 
         // Header tabla RESULTADOS:
         System.out.println("\nTABLA DE RESULTADOS:\n");
@@ -36,42 +36,56 @@ public class Main {
         // Contenido de la tabla RESULTADOS:
         for (String[] esteResultado : resultados) {
 
+            String ganadorPartido = "-";
+
             // guardo los valores en partido
             Partido partido = new Partido(esteResultado[2], esteResultado[4], esteResultado[5], esteResultado[3]);
 
             // guardo el resultadoGanador de cada partido tomando los datos de resultados
             resultadosGanador.add(partido.getResultado());
 
+            // Reemplazo el valor por defecto "-" (EMPATE), por el nombre del equipo GANADOR
+            if (partido.getResultado() == 1) {
+                ganadorPartido = esteResultado[2];
+            } else if (partido.getResultado() == 2) {
+                ganadorPartido = esteResultado[3];
+            }
+
             // muestro los datos de resultado + GANADOR
-            System.out.printf(formatoTablaResultados, esteResultado[0], esteResultado[1], esteResultado[2], esteResultado[3], esteResultado[4], esteResultado[5], partido.getResultado());
+            System.out.printf(formatoTablaResultados, esteResultado[0], esteResultado[1], esteResultado[2], esteResultado[3], esteResultado[4], esteResultado[5], ganadorPartido);
         }
 
         // Muestro los datos por separado
         System.out.printf("\n%15s %s %n", "PARTICIPANTES:", participantes);
         System.out.printf("%15s %s %n", "FASES:", fases);
         System.out.printf("%15s %s %n", "RONDAS:", rondas);
-        //System.out.printf("%15s %s %n","RESULTADO:",resultadosGanador);
-        System.out.println(" ");
-        System.out.printf("%15s %s %n", "RESULTADO:", "");
+        // System.out.printf("%15s %s %n","RESULTADO:",resultadosGanador);
 
 
         // BUCLE comparando resultadosGanador con los pronósticos de cada participante.
+        System.out.println(" ");
+        System.out.printf("%15s %n", "RESULTADOS:");
+
         for (int i = 0; i < participantes.size(); i++) {
+
             // Creo un objeto por participante
             Pronostico pronostico = new Pronostico(cantidadParticipantes.getParticipantes().get(i));
             participantePronostico = pronostico.getPronostico();
 
             // Comparo resultadoGanador con cada participante
+            System.out.println("---------------------------------------");
+            System.out.printf("%35s %n", cantidadParticipantes.getParticipantes().get(i) + ": ");
+
             ResultadoEnum resultadoEnum = new ResultadoEnum(resultadosGanador, participantePronostico);
-            System.out.printf("%15s %s %n", cantidadParticipantes.getParticipantes().get(i) + ": ", resultadoEnum.getPuntos());
+            System.out.printf("%35s %s %n", "TOTAL DE PUNTOS OBTENIDOS: ", resultadoEnum.getPuntos());
 
             // List Pronostico del participante:
             // System.out.printf("%15s %s %n","PRONOSTICO:",cantidadParticipantes.getParticipantes().get(i) + ": " + participantePronostico);
 
             // Resultados:
             // Mariana: 20, Pedro:24, Juan: 20, Sofia: 21.
-            // Sofia acierta completa la ronda final.
-            // Nadie acierta completa la fase
+            // Sofia completa solo la ronda final.
+            // Nadie completa una fase.
         }
 
     }
--- src/ResultadoEnum.java
+++ src/ResultadoEnum.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -7,10 +7,16 @@ public class ResultadoEnum {
     private List<String[]> resultados = DatosSQL.leerResultados();
 
     private int puntos;
+    private int partidoNum;
+
+    private Ronda rondas = new Ronda();
+    private Fase fases = new Fase();
+
 
     private int puntosPartido = 1;
     private int puntosExtraRonda = 5;
     private int puntosExtraFase = 10;
+
 
     List<Integer> resultado;
     List<Integer> pronostico;
@@ -28,15 +34,59 @@ public class ResultadoEnum {
 
         puntos = 0;
 
-        for (int i = 0; i < resultado.size(); i++) {
-            if (Objects.equals(resultado.get(i), pronostico.get(i))) {
+        partidoNum = 0;
+
+        List<String> rc = rondas.getRondas();
+        List<String> fc = fases.getFases();
+
+        Integer[] rondasCompletas = new Integer[rc.size()];
+        Integer[] fasesCompletas = new Integer[fc.size()];
+
+
+
+        for (String[] cantidadPartidos : this.resultados) {
+
+            // chequeo los puntos por partido
+            if (!Objects.equals(resultado.get(partidoNum), pronostico.get(partidoNum))) {
+
+                // defino 0 posicion de rondas y fases
+                fasesCompletas[fc.indexOf(cantidadPartidos[0])] = 0;
+                rondasCompletas[rc.indexOf(cantidadPartidos[1])] = 0;
+
+            }else{
+                // defino 1 posicion de rondas y fases
+                if(!Objects.nonNull(fasesCompletas[fc.indexOf(cantidadPartidos[0])]) || fasesCompletas[fc.indexOf(cantidadPartidos[0])] == 1) {
+                    fasesCompletas[fc.indexOf(cantidadPartidos[0])] = 1;
+                }
+                if(!Objects.nonNull(rondasCompletas[rc.indexOf(cantidadPartidos[1])]) || rondasCompletas[rc.indexOf(cantidadPartidos[1])] == 1) {
+                    rondasCompletas[rc.indexOf(cantidadPartidos[1])] = 1;
+                }
+                // sumo un punto de partido acertado.
                 puntos += puntosPartido;
             }
+
+            partidoNum++;
+
         }
 
-        // FALTA SUMAR puntosExtraRonda Y puntosExtraFase.
+        System.out.printf("%35s %s %n","Fases completas (+ "+puntosExtraFase+" puntos): ",sumaDatos(fasesCompletas));
+        System.out.printf("%35s %s %n","Rondas completas (+ "+puntosExtraRonda+" puntos): ",sumaDatos(rondasCompletas));
+
+        // sumo puntos totales en caso de completar fases y/o rondas
+        puntos += (sumaDatos(fasesCompletas) * puntosExtraFase) + (sumaDatos(rondasCompletas) * puntosExtraRonda);
 
         return puntos;
+    }
+
+    private Integer sumaDatos(Integer[] dato){
+
+        int resultadoSuma = 0;
+
+        for (int i: dato) {
+            resultadoSuma += i;
+        }
+
+        return resultadoSuma;
     }
 
     public int getPuntos() {
